1cddcc45968c50fd2f0684fb49634e3cc027efde
Lyolya <almasilaura@outlook.com>
2021-02-09 19:44:27 +0000

    FINISHED Extentions

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -19,6 +19,17 @@ public class BorrowerTest {
     @Test
     public void canBorrow(){
         borrower.borrowBook(book);
+        borrower.borrowBook(book);
+        borrower.borrowBook(book);
+        assertEquals(3, borrower.countBorrowedBooks());
+    }
+
+    @Test
+    public void canReturnBook(){
+        borrower.borrowBook(book);
+        borrower.borrowBook(book);
+        borrower.returnBook(book);
         assertEquals(1, borrower.countBorrowedBooks());
+
     }
 }
