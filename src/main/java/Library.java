b45b5e2635935c58dc5ba36b67ebb9e908dac0f2
Lyolya <almasilaura@outlook.com>
2021-02-09 18:22:45 +0000

    SET UP Library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Library {
+    private ArrayList<Book> books;
+    private int stockCapacity;
+//    private HashMap<String, String>
+
+
+    public Library(int stockCapacity){
+        this.stockCapacity = stockCapacity;
+        this.books = new ArrayList<>();
+    }
 }
