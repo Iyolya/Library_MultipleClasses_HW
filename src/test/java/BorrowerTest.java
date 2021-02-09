8594f3ca155695710faec45d379b0937433fdb44
Lyolya <almasilaura@outlook.com>
2021-02-09 19:31:44 +0000

    TEST canBorrow for class Borrower PASSED

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,3 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
 
+    private Library library;
+    private Book book;
+    private Borrower borrower;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book = new Book("The Red Book", "Carl Gustav Jung", "Biography");
+        borrower = new Borrower("Laura");
+    }
+
+    @Test
+    public void canBorrow(){
+        borrower.borrowBook(book);
+        assertEquals(1, borrower.countBorrowedBooks());
+    }
 }
