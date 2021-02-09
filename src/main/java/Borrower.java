48624584dc3dc869f05c34b583d56857830f529f
Lyolya <almasilaura@outlook.com>
2021-02-09 19:19:10 +0000

    SET UP Borrower class

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private String name;
+    private ArrayList<Book> borrowedBooks;
+
+    public Borrower(String name){
+        this.name = name;
+        this. borrowedBooks = new ArrayList<>();
+    }
 }
+
+
